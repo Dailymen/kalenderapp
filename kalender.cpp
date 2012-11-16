90fddc04d4b5df2e26b89440d692e297c72b136e
Sindre Myrmehl Teigland <sindre.teigland@gmail.com>
2012-11-16 17:59:56 +0100

    endret på saker

--- kalender.cpp
+++ kalender.cpp
@@ -1,15 +1,34 @@
 #include "kalender.h"
 #include "ui_kalender.h"
+#include <QtGui>
+#include <QDebug>
 
 kalender::kalender(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::kalender)
 {
-    ui->setupUi(this);
 
+    ui->setupUi(this);
+    connect(ui->matiasknapp, SIGNAL(clicked()), this, SLOT(matias()));
 }
 
 kalender::~kalender()
 {
     delete ui;
+}
+
+
+
+
+void kalender::matias()
+{
+    ui->dager->setText("new Text");
+}
+
+
+
+void kalender::opprett()
+{
+    ui->Kontaktinfo
+
 }
--- kalender.h
+++ kalender.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QWidget>
+#include <QtGui>
 
 namespace Ui {
 class kalender;
@@ -18,6 +19,13 @@ public:
     
 private:
     Ui::kalender *ui;
+
+signals:
+
+
+private slots:
+    void matias();
+    void opprett();
 };
 
 #endif // KALENDER_H
